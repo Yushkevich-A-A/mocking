3897931d60acddb995c4da8451f2ec01e9dc6fe6
Yushkevich-A-A <Mortifer1993@yandex.ru>
2021-04-26 00:13:10 +0300

    lint without mistakes

---
+++ src/js/__tests__/level.test.js
@@ -0,0 +1,32 @@
+import fetchData from '../http';
+import { getLevel } from '../level';
+
+jest.mock('../http');
+
+beforeEach(() => {
+  jest.resetAllMocks();
+});
+
+test('call loadUserLevel with successful', () => {
+  fetchData.mockReturnValue({ status: 'ok', level: 80 });
+
+  const result = getLevel(1);
+  expect(result).toBe('Ваш текущий уровень: 80');
+  expect(fetchData).toBeCalledWith('https://server/user/1');
+});
+
+test('call loadUserLevel with empty result', () => {
+  fetchData.mockReturnValue({});
+
+  const result = getLevel(1);
+  expect(result).toEqual('Информация об уровне временно недоступна');
+  expect(fetchData).not.toBeCalledWith('https://server/user/2');
+});
+
+test('call loadUserLevel with error result', () => {
+  fetchData.mockReturnValue({ status: 'error' });
+
+  const result = getLevel(1);
+  expect(result).toEqual('Информация об уровне временно недоступна');
+  expect(fetchData).toBeCalledWith('https://server/user/1');
+});
--- src/js/level.js
+++ src/js/level.js
@@ -2,11 +2,11 @@ import fetchData from './http';
 
 export function getLevel(userId) {
   const response = fetchData(`https://server/user/${userId}`);
-  
+
   // TODO: логика обработки
   if (response.status === 'ok') {
-     return `Ваш текущий уровень: ${response.level}`; 
+    return `Ваш текущий уровень: ${response.level}`;
   }
-  
-  return `Информация об уровне временно недоступна`;
+
+  return 'Информация об уровне временно недоступна';
 }
